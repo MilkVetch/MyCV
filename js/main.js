1a7e7ebccd781bf8cc3e52e94f2330e106d9c3a9
milkvetch <457804000@qq.com>
2016-01-13 13:46:49 +1300

    finish s-about
    + fix menu btn issue
    + new content
    + interest part responsive setting

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+// if ($(window).width() < 695) {
+// 	alert('!');
+// }
 // aside
 $('.toggle-btn').click(function() {
 	$(this).toggleClass('menu-open');
 	$('aside nav').toggleClass('menu-open');
-	$('.btn-ctr').show();
+	$('.btn-ctr').toggle();
 });
 $('.btn-ctr').click(function() {
 	$('.toggle-btn').toggleClass('menu-open');
@@ -16,3 +19,25 @@ $('aside nav ul li').click(function() {
 		$('.btn-ctr').hide();
 	},300);
 });
+
+// about me
+$('.interest-box i').mouseover(function() {
+	$('.int-des-0').hide();
+	var intid = $(this).data('intid');
+	$('.int-des-'+intid).show();
+	$('.about .image').css("background-image", "url(img/int"+intid+".jpg)");
+	var h = $('.about-content').height()+$('.about-title').height();
+	if ($(window).width() <= 695) {
+		$('.about .image').css("transform","translateY("+h+"px)");
+	}
+
+}).mouseout(function() {
+	$('.int-des-0').show();
+	var intid = $(this).data('intid');
+	$('.int-des-'+intid).hide();
+	$('.about .image').css("background-image", "url(img/sketch.jpg)");
+	if ($(window).width() <= 695) {
+		$('.about .image').css("transform","translateY(0px)");
+	}
+});
+
